96eaf66e232ac92514b1acc300d4179073cbc7ed
Jordan MacDonald <jordan@wastedintelligence.com>
2013-11-06 00:38:43 -0500

    Use Elixir 0.11.0.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Transcript.Mixfile do
   def project do
     [ app: :transcript,
       version: "0.0.1",
-      elixir: "~> 0.10.3",
+      elixir: "~> 0.11.0",
       deps: deps ]
   end
 
