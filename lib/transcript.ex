fb91d3e0055cc1603db8a25507d5c239f48a9f95
Jordan MacDonald <jordan@wastedintelligence.com>
2013-11-22 01:01:29 -0500

    Parse other node argument, connecting to it if set.

--- lib/transcript.ex
+++ lib/transcript.ex
@@ -14,6 +14,19 @@ defmodule Transcript do
     # Set the node name for distribution.
     :net_kernel.start [:"#{node_name}", :shortnames]
 
+    IO.puts "Registered as #{Node.self}."
+
+    # Connect to another node, if specified.
+    unless other_args == [] do
+      # Parse the other node name.
+      [ other_node_name | _ ] = other_args
+
+      # Try to connect to the other node.
+      if Node.connect :"#{other_node_name}" do
+        IO.puts "Connected to #{other_node_name} successfully."
+      end
+    end
+
     # Start the server on the specified port.
     Server.start port
   end
